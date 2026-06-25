495a09dcff9c5a5667c8408d6d044f086a352db5
ming694259021-cpu <694259021@webmail.npru.ac.th>
2026-06-25 16:29:49 +0700

    first commit

---
+++ nameage.java
@@ -0,0 +1,7 @@
+void main() {
+    IO.print("Enter your name :");
+    String name = IO.readln();
+    IO.print("Enter your age");
+    int age = Integer.parseInt(IO.readln());
+    IO.println("Hello! " + name + ", Your age is " + age);
+}
