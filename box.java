72ad27896b3fbcbf7ba835d905c024f776358883
ming694259021-cpu <694259021@webmail.npru.ac.th>
2026-06-25 16:49:15 +0700

    first commit

---
+++ box.java
@@ -0,0 +1,8 @@
+void main() {
+    IO.print("Enter number of box : ");
+    int box = Integer.parseInt(IO.readln());
+    IO.print("Enter of your pencil : ");
+    int pencil = Integer.parseInt(IO.readln());
+    int anwser = box * pencil;
+    IO.println("Total number of pencils = " + anwser);
+}
